b3b23db57036e4e99eba67d5718c32f59f4fe954
vbelous <vbbelous@gmail.com>
2018-08-30 20:00:14 +0300

    2.*

---
+++ Lesson_01/2.1.py
@@ -0,0 +1,7 @@
+x = int(input("input x: "))
+y = int(input("input y: "))
+print("x = ", x, "y = ", y)
+x = x + y
+y = x - y
+x = x - y
+print("x = ", x, "y = ", y)
---
+++ Lesson_01/2.py
@@ -0,0 +1,7 @@
+x = input("input x: ")
+y = input("input y: ")
+print("x = ", x, "y = ", y)
+z = x
+x = y
+y = z
+print("x = ", x, "y = ", y)
