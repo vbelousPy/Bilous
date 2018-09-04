ce51649c002b2ca10b805a837c9656f6d42e7840
vbelous <vbbelous@gmail.com>
2018-09-04 12:01:58 +0300

    1

---
+++ Lesson_02/1.py
@@ -0,0 +1,15 @@
+import math
+
+a = int(input("Enter a: "))
+b = int(input("Enter b: "))
+c = int(input("Enter c: "))
+
+d = b ** 2 - 4 * a * c
+if d < 0:
+    print("The result is a complex value")
+else:
+    if d == 0:
+        print("x =", (-b / 2 * a))
+    else:
+        print("x1 =", ((-b - math.sqrt(d)) / 2 * a))
+        print("x2 =", ((-b + math.sqrt(d)) / 2 * a))
