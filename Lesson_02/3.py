91d1e1e3d9703cf6fbd2474ac5023d31129d8524
vbelous <vbbelous@gmail.com>
2018-09-04 13:12:58 +0300

    3

---
+++ Lesson_02/3.py
@@ -0,0 +1,17 @@
+n = int(input("Enter n: "))
+if n <= 0:
+    print("Fibonacci only for a number > 0")
+else:
+    if n == 1:
+        print("result = 0")
+    else:
+        prevResult = 0
+        result = 1
+        tmp = 0
+        i = 2
+        while i < n:
+            tmp = result
+            result += prevResult
+            prevResult = tmp
+            i += 1
+        print("result =", result)
