b5ed82d8dc7b5c4c8f5e254d65e431531cf7b6bc
vbelous <vbbelous@gmail.com>
2018-09-25 09:37:26 +0300

    5

---
+++ Lesson_05/5.py
@@ -0,0 +1,15 @@
+def foo(*args, **kwargs):
+    my_list = list(args)
+    for i in range(len(my_list)):
+        my_list[i] = my_list[i] + 1
+    my_dict = kwargs
+    for key in my_dict.keys():
+        my_dict.update({key: my_dict.get(key) + 1})
+    return my_list, my_dict
+
+
+def runner(some_func, *args, **kwargs):
+    return some_func(*args, **kwargs)
+
+
+print(runner(foo, 1, 2, 3, a=4, b=5))
