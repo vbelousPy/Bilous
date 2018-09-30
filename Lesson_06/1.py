22fd1ee88a6cea0fe9fe44d4cfe373d8af22e51f
vbelous <vbbelous@gmail.com>
2018-09-30 11:02:03 +0300

    1

---
+++ Lesson_06/1.py
@@ -0,0 +1,24 @@
+class Queue:
+
+    def __init__(self, queue_list):
+        self.queue_list = [i for i in queue_list]
+
+    def pop(self):
+        if len(self.queue_list) > 0:
+            return self.queue_list[0]
+        else:
+            raise IndexError
+
+    def push(self, new_element):
+        self.queue_list.append(new_element)
+
+    def __str__(self) -> str:
+        return str(self.queue_list)
+
+
+test_queue = Queue(["a", "b", "c"])
+print(test_queue)
+print(test_queue.pop())
+test_queue.push("f")
+print(test_queue)
+
